e4e5225fdf6a0c170f8c17df0f384bd43685e349
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-06-19 14:07:12 -0500

    feat: implement CRUD functionality for Tarea model

--- todos/urls.py
+++ todos/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import TareaViewSet
-
-router = DefaultRouter()
-router.register(r'tareas', TareaViewSet)
+from django.urls import path
+from todos import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', views.tarea_list, name='tarea_list'),
+    path('create/', views.tarea_create, name='tarea_create'),
+    path('update/<int:pk>/', views.tarea_update, name='tarea_update'),
+    path('delete/<int:pk>/', views.tarea_delete, name='tarea_delete'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarea
-from .serializers import TareaSerializer
+from .forms import TareaForm
 
-class TareaViewSet(viewsets.ModelViewSet):
-    queryset = Tarea.objects.all().order_by('-id')
-    serializer_class = TareaSerializer
+def tarea_list(request):
+    tareas = Tarea.objects.all().order_by('-id')
+    return render(request, 'todos/list.html', {'tareas': tareas})
+
+def tarea_create(request):
+    if request.method == 'POST':
+        form = TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tarea_list')
+    else:
+        form = TareaForm()
+    return render(request, 'todos/create.html', {'form': form})
+
+def tarea_update(request, pk):
+    tarea = get_object_or_404(Tarea, pk=pk)
+    if request.method == 'POST':
+        form = TareaForm(request.POST, instance=tarea)
+        if form.is_valid():
+            form.save()
+            return redirect('tarea_list')
+    else:
+        form = TareaForm(instance=tarea)
+    return render(request, 'todos/update.html', {'form': form})
+
+def tarea_delete(request, pk):
+    tarea = get_object_or_404(Tarea, pk=pk)
+    tarea.delete()
+    return redirect('tarea_list')
